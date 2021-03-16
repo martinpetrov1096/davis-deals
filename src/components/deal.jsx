a3ccd705d702117103b816a43a71488c1f78980b
Martin Petrov <mlpetrov@ucdavis.edu>
2021-03-15 18:00:08 -0700

    added some more deals, and added pricing to deals.jsx

--- src/components/deal.jsx
+++ src/components/deal.jsx
@@ -12,6 +12,7 @@ export default function Deal({ deal }) {
 
    return (
       <Wrapper>
+         <Time>{deal.timeStart + '-' + deal.timeEnd}</Time>
          <Description>{deal.description}</Description>
          <Price>{deal.price}</Price>
       </Wrapper>
@@ -34,9 +35,19 @@ const Wrapper = styled.div`
    flex-flow: row nowrap;
    justify-content: space-between;
 `;
+const Time = styled.h4`
+   flex-shrink: 0;
+   flex-grow: 0;
+   width: 75px;
+   margin-right: 5px;
+   font-size: min(14px, 3vw);
+`;
 const Description = styled.h4`
    font-size: min(18px, 4vw);
+   text-align: center;
+   line-height: 1.3;
 `;
 const Price = styled.h4`
+   margin-left: 5px;
    font-size: min(18px, 4vw);
 `;
